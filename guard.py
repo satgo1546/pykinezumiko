c43891d9af9fbb4a1b94e3598475d9cbea0c63de
akhia <akhialomgir362856@gmail.com>
2023-01-03 17:51:43 +0800

    schedule at:Tue Jan  3 17:51:43 2023

--- guard.py
+++ guard.py
@@ -3,20 +3,14 @@ import time
 import schedule
 import subprocess
 
-#TODO   git pull
-
-#TODO   git add .
-#       git commit 
-#       git push
-
-def a():
+def checkPullThenPush():
     if subprocess.check_output("git pull", shell=True)==b'Already up to date.\n':
         os.system("git add .")
         os.system("git commit -m 'schedule at:"+time.ctime()+"'")
         os.system("git push")
     print(time.ctime())
 
-schedule.every(0.01).minutes.do(a)
+schedule.every(0.01).minutes.do(checkPullThenPush)
 #schedule.every(1).hour.do(a)
 
 while True:
