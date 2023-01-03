68408147e7c0c9788f79e6abd01ceb0bb381c2d6
Frog Chen <satgo1546@yeah.net>
2023-01-03 21:01:10 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Hello World!'
+    return "Goodbye World!"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
