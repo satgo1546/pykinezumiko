6e8e33d8ba0e6636d22dcbdbe49a336c5ca3f485
akhia <akhialomgir362856@gmail.com>
2023-01-03 20:26:07 +0800

    schedule at:Tue Jan  3 20:26:07 2023

--- guard.py
+++ guard.py
@@ -3,18 +3,23 @@ import logging
 import schedule
 import subprocess
 
+
 def checkPullThenPush():
     logger.info(subprocess.getoutput("git pull"))
     logger.info(subprocess.getoutput("git add ."))
-    logger.info(subprocess.getoutput("git commit -m 'schedule at:"+time.ctime()+"'"))
+    logger.info(subprocess.getoutput(
+        "git commit -m 'schedule at:"+time.ctime()+"'"))
     logger.info(subprocess.getoutput("git push"))
     print(time.ctime())
 
-logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    filename='guard.log',
+                    filemode='a')
 logger = logging.getLogger(__name__)
 
 schedule.every(0.01).minutes.do(checkPullThenPush)
-#schedule.every(1).hour.do(a)
+# schedule.every(1).hour.do(a)
 
 while True:
     schedule.run_pending()
