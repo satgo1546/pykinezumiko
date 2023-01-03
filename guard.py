f50dac0459725a3fd794d248961277acd3e421b8
akhia <akhialomgir362856@gmail.com>
2023-01-03 20:27:37 +0800

    schedule at:Tue Jan  3 20:27:37 2023

--- guard.py
+++ guard.py
@@ -14,6 +14,7 @@ def checkPullThenPush():
 
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.DEBUG,
                     filename='guard.log',
                     filemode='a')
 logger = logging.getLogger(__name__)
