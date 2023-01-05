80940418ed4fbb0842fec5e7609c56246156d1f4
akhia <akhialomgir362856@gmail.com>
2023-01-05 15:14:13 +0800

    debug=True

--- demo.py
+++ demo.py
@@ -1,9 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return "Goodbye World!"
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
