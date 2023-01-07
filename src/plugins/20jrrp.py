56ae0b6dfb8aaeef42c7d0e53cc49f52d5d805ac
akhialomgir <akhialomgir362856@gmail.com>
2023-01-07 20:32:25 +0800

    实现了jrrp

--- src/plugins/20jrrp.py
+++ src/plugins/20jrrp.py
@@ -1,7 +1,9 @@
+import time
+import random
 from .. import ChatbotBehavior
-
 
 class 今日人品(ChatbotBehavior):
     def on_message(self, context: int, sender: int, text: str, message_id: int):
         if text == ".jrrp":
-            return "诶，功能还没做呢！"
+            random.seed((int(time.time()) + 3600 * 8) // 86400 + sender)
+            return random.randint(0, 100)
