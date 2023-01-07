ba55ba509b82a40b4b6dad1a9c7e8aaeb9d86ad0
akhialomgir <akhialomgir362856@gmail.com>
2023-01-07 20:43:03 +0800

    return int>str

--- src/plugins/20jrrp.py
+++ src/plugins/20jrrp.py
@@ -6,4 +6,4 @@ class 今日人品(ChatbotBehavior):
     def on_message(self, context: int, sender: int, text: str, message_id: int):
         if text == ".jrrp":
             random.seed((int(time.time()) + 3600 * 8) // 86400 + sender)
-            return random.randint(0, 100)
+            return str(random.randint(0, 100))
