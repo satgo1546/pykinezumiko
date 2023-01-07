22a5514c7ee10a63482f0d3caf97f870e278d11a
akhialomgir <akhialomgir362856@gmail.com>
2023-01-07 21:34:02 +0800

    独立化随机种子

--- src/plugins/20jrrp.py
+++ src/plugins/20jrrp.py
@@ -5,5 +5,5 @@ from .. import ChatbotBehavior
 class 今日人品(ChatbotBehavior):
     def on_message(self, context: int, sender: int, text: str, message_id: int):
         if text == ".jrrp":
-            random.seed((int(time.time()) + 3600 * 8) // 86400 + sender)
-            return str(random.randint(0, 100))
+            t = random.Random((int(time.time()) + 3600 * 8) // 86400 + sender)
+            return "今日のあんたん運勢は"+str(t.randint(0, 100))+"点や"
