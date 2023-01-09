a33bef517854c926dcb91603220891d9bac26e98
Frog Chen <satgo1546@yeah.net>
2023-01-09 18:42:51 +0800

    移动normalize_command_name和parse_command到humanity模块

--- src/__init__.py
+++ src/__init__.py
@@ -1,14 +1,13 @@
 import inspect
-import re
 import time
-import unicodedata
 from bisect import bisect_left
 from collections import OrderedDict
 from collections.abc import Generator
-from itertools import filterfalse, groupby
 from typing import Any, ClassVar, Optional, Union, overload
 
 import requests
+
+from .humanity import normalize_command_name, parse_command
 
 
 class ChatbotBehavior:
@@ -227,61 +226,6 @@ class ChatbotBehavior:
         self._name_cache[context] = name
         return name
 
-    @staticmethod
-    def normalize_command_name(text: str) -> list[str]:
-        """当输入字符串以命令符（句点“.”、句号“。”、叹号“!”或“！”）开头，给出按命令名词法切分后的列表，否则返回空列表。
-
-        将对输入字符串"! Ｆｏｏ  BÄR114514 "进行下述Unicode变换。
-
-        - 丢弃开头的命令符，且只取字符串的开头一段。
-            → " Ｆｏｏ  BÄR114514 "
-        - 消去开头和结尾的空白符。
-            → "Ｆｏｏ  BÄR114514"
-        - 替换连续的空白符和下划线为单个下划线。
-            → "Ｆｏｏ_BÄR114514"
-        - case folding。简单地说就是变成小写。一些语言有额外变换（ß → ss，ς → σ等）。
-            → "ｆｏｏ_bär114514"
-        - 兼容分解形式标准化（NFKD）。简单地说就是把怪字转换为正常字，比如全角变成半角。
-            → "foo_ba\u0308r114514"
-        - 删去组合字符。一些语言的语义可能受到影响（é → e，が → か等）。
-            → "foo_bar114514"
-        - 按字符类别分组。
-            → ["foo", "_", "bar", "114514"]
-        """
-        return (
-            [
-                "".join(s)
-                for _, s in groupby(
-                    filterfalse(
-                        unicodedata.combining,
-                        unicodedata.normalize(
-                            "NFKD",
-                            re.sub(r"[\s_]+", "_", text[1:111].strip()).casefold(),
-                        ),
-                    ),
-                    unicodedata.category,
-                )
-            ]
-            if text[0] in ".。!！"
-            else []
-        )
-
-    @staticmethod
-    def parse_command(
-        parameters: dict[str, inspect.Parameter],
-        given_arguments: dict[str, Any],
-        text: str,
-    ) -> dict[str, Any]:
-        """根据函数签名从命令名之后的字符串中宽容地解析参数。
-
-        梦里才能用！
-        """
-        # TODO
-        for name, parameter in parameters.items():
-            parameters[name]
-            parameter.annotation
-        raise NotImplementedError({})
-
     def dispatch_command(
         self, context: int, sender: int, text: str, message_id: int
     ) -> object:
@@ -300,13 +244,17 @@ class ChatbotBehavior:
         on_command_×××方法的参数必须是。
         """
         arguments = locals().copy()
-        parts = self.normalize_command_name(text)
+        parts = normalize_command_name(text)
         while parts:
             f = getattr(self, "on_command_" + "".join(parts), None)
             if callable(f):
                 return f(
-                    **self.parse_command(
-                        dict(inspect.signature(f).parameters),
+                    **parse_command(
+                        {
+                            parameter.name: parameter.annotation
+                            for parameter in inspect.signature(f).parameters.values()
+                            if parameter.annotation is not parameter.empty
+                        },
                         arguments,
                         # 在原始字符串中找到命令名之后的部分。
                         # 证明一下这个二分法数据的单调性？
@@ -316,7 +264,7 @@ class ChatbotBehavior:
                             bisect_left(
                                 range(1, 111),
                                 parts,
-                                key=lambda i: self.normalize_command_name(text[:i]),
+                                key=lambda i: normalize_command_name(text[:i]),
                             ) :
                         ].strip(),
                     ),
--- src/humanity.py
+++ src/humanity.py
@@ -1,4 +1,7 @@
-from typing import SupportsInt
+import re
+import unicodedata
+from itertools import filterfalse, groupby
+from typing import Any, SupportsInt
 
 
 def format_timespan(seconds: SupportsInt) -> str:
@@ -19,3 +22,62 @@ def format_timespan(seconds: SupportsInt) -> str:
     r.append(str(seconds))
     r.append("秒")
     return " ".join(r)
+
+
+def normalize_command_name(text: str) -> list[str]:
+    """当输入字符串以命令符（句点“.”、句号“。”、叹号“!”或“！”）开头，给出按命令名词法切分后的列表，否则返回空列表。
+
+    将对输入字符串"! Ｆｏｏ  BÄR114514 "进行下述Unicode变换。
+
+    - 丢弃开头的命令符，且只取字符串的开头一段。
+        → " Ｆｏｏ  BÄR114514 "
+    - 消去开头和结尾的空白符。
+        → "Ｆｏｏ  BÄR114514"
+    - 替换连续的空白符和下划线为单个下划线。
+        → "Ｆｏｏ_BÄR114514"
+    - case folding。简单地说就是变成小写。一些语言有额外变换（ß → ss，ς → σ等）。
+        → "ｆｏｏ_bär114514"
+    - 兼容分解形式标准化（NFKD）。简单地说就是把怪字转换为正常字，比如全角变成半角。
+        → "foo_ba\u0308r114514"
+    - 删去组合字符。一些语言的语义可能受到影响（é → e，が → か等）。
+        → "foo_bar114514"
+    - 按字符类别分组。
+        → ["foo", "_", "bar", "114514"]
+    """
+    return (
+        [
+            "".join(s)
+            for _, s in groupby(
+                filterfalse(
+                    unicodedata.combining,
+                    unicodedata.normalize(
+                        "NFKD",
+                        re.sub(r"[\s_]+", "_", text[1:111].strip()).casefold(),
+                    ),
+                ),
+                unicodedata.category,
+            )
+        ]
+        if text[0] in ".。!！"
+        else []
+    )
+
+
+TYPES = {k: re.compile(v) for k, v in {int: r""}.items()}
+
+
+def parse_command(
+    parameters: dict[str, type],
+    given_arguments: dict[str, Any],
+    text: str,
+) -> dict[str, Any]:
+    """根据函数签名从命令名之后的字符串中宽容地解析参数。
+
+    梦里才能用！
+    """
+    # TODO
+    kwargs = {k: v for k, v in given_arguments.items() if k in parameters}
+    for name, parameter in parameters.items():
+        parameters[name]
+    raise NotImplementedError({})
+    return kwargs
