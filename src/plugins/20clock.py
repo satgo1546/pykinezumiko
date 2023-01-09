e3f8920a8b3f310a69125491557978abc37d7da5
akhialomgir <akhialomgir362856@gmail.com>
2023-01-09 12:06:43 +0800

    修复部分问题 并分别对收尾进行判别

--- src/plugins/20clock.py
+++ src/plugins/20clock.py
@@ -11,17 +11,28 @@ class Clock(ChatbotBehavior):
 
     def on_message(self, context: int, sender: int, text: str, message_id: int):
         if text.startswith(".clock"):
-            # .clock 50 v我50
+            # ".clock 增加的时间 消息" or ".clock 消息 增加的时间"
             dtAndTitle = text.split(" ")[1:]
-            dt = int(dtAndTitle[0])
-            title = dtAndTitle[1]
+            # 1 匹配开头作为时间输入
+            dt, title = None, " ".join(dtAndTitle[1:])
+            # 匹配int数字
+            if dtAndTitle[0].isdigit():
+                dt = int(dtAndTitle[0])
+            # 2 匹配结尾作为时间输入
+            title = " ".join(dtAndTitle[:-1])
+            # 匹配int数字
+            if dtAndTitle[-1].isdigit():
+                dt = int(dtAndTitle[-1])
             # 存储格式：[浮点触发时间戳，回复内容，会话id]
-            self.q.append([time.time()+dt, title, context])
-            return str(time.time()+dt)+" "+title
+            if dt:
+                self.q.append([time.time()+dt, title, context])
+                return str(time.time()+dt)+" "+title
+            else:
+                return "无法识别到有效时间"
 
     def on_interval(self):
         # 如果提醒队列非空且第一个提醒到时间了就提醒用户
         while self.q and self.q[0][0] < time.time():
-            title=self.q[0][1]
-            self.send(self.q.pop()[2], title)
+            title = self.q[0][1]
+            self.send(self.q.popleft()[2], title)
             time.sleep(1)
