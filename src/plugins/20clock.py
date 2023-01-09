d4052d8b0e7a1d85d2d6a39b884fb37c078d0c7b
akhialomgir <akhialomgir362856@gmail.com>
2023-01-09 20:25:04 +0800

    use re

--- src/plugins/20clock.py
+++ src/plugins/20clock.py
@@ -1,3 +1,4 @@
+import re
 import os
 import time
 import pickle
@@ -11,10 +12,10 @@ class Clock(ChatbotBehavior):
         # 存储路径
         self.path = '20clock.pickle'
         # 提醒队列
-        t=[]
+        t = []
         if os.path.isfile(self.path):
             with open(self.path, 'rb') as f:
-                t=pickle.load(f)
+                t = pickle.load(f)
         self.pq = PriorityQueue()
         for l in t:
             self.pq.put(l)
@@ -22,29 +23,26 @@ class Clock(ChatbotBehavior):
     def on_message(self, context: int, sender: int, text: str, message_id: int):
         if text.startswith(".clock"):
             # ".clock 增加的时间 消息" or ".clock 消息 增加的时间"
-            dtAndTitle = text.split(" ")[1:]
-            if not dtAndTitle: return "无法识别到有效时间"
-            # 1 匹配开头作为时间输入
-            dt = None
-            # 匹配int数字
-            if not dt and dtAndTitle[0].isdigit():
-                title = " ".join(dtAndTitle[1:]).strip()
-                dt = int(dtAndTitle[0])
-            # 2 匹配结尾作为时间输入
-            # 匹配int数字
-            if not dt and dtAndTitle[-1].isdigit():
-                title = " ".join(dtAndTitle[:-1]).strip()
-                dt = int(dtAndTitle[-1])
-            # 存储格式：[浮点触发时间戳，回复内容，会话id]
-            if dt and title:
-                self.pq.put([time.time()+dt, title, context])
-                with open(self.path, 'wb') as f:
-                    pickle.dump(list(self.pq.queue), f)
-                return str(time.time()+dt)+" "+title
-            elif dt and not title:
-                return "标题不能为空"
-            else:
+            dtAndTitle = text[7:]
+            # 匹配开头和结尾作为时间输入
+            title, dt = None, None
+            res = re.search(r"^\d+\s|\s\d+$", dtAndTitle)
+            if not res:
                 return "无法识别到有效时间"
+            else:
+                l, r = res.span()
+                dt = int(res.group().strip())
+                title = dtAndTitle[:l]+dtAndTitle[r:]
+                title = title.strip()
+                if not title:
+                    return "标题不能为空"
+
+            # 存储格式：[浮点触发时间戳，回复内容，会话id]
+            self.pq.put([time.time()+dt, title, context])
+            with open(self.path, 'wb') as f:
+                pickle.dump(list(self.pq.queue), f)
+            return str(time.time()+dt)+" "+title
+
 
     def on_interval(self):
         # 如果提醒队列非空且第一个提醒到时间了就提醒用户
