9ccacbd7d5d3ec285b6d9d9e4b2fd40a67c09671
akhialomgir <akhialomgir362856@gmail.com>
2023-01-09 15:24:42 +0800

    增加了文件存储

--- src/plugins/20clock.py
+++ src/plugins/20clock.py
@@ -1,4 +1,6 @@
+import os
 import time
+import pickle
 from queue import PriorityQueue
 from .. import ChatbotBehavior
 
@@ -6,8 +8,16 @@ from .. import ChatbotBehavior
 class Clock(ChatbotBehavior):
     def __init__(self) -> None:
         super().__init__()
+        # 存储路径
+        self.path = '20clock.pickle'
         # 提醒队列
+        t=[]
+        if os.path.isfile(self.path):
+            with open(self.path, 'rb') as f:
+                t=pickle.load(f)
         self.pq = PriorityQueue()
+        for l in t:
+            self.pq.put(l)
 
     def on_message(self, context: int, sender: int, text: str, message_id: int):
         if text.startswith(".clock"):
@@ -26,13 +36,17 @@ class Clock(ChatbotBehavior):
                 title = " ".join(dtAndTitle[:-1])
                 dt = int(dtAndTitle[-1])
             # 存储格式：[浮点触发时间戳，回复内容，会话id]
-            if dt:
+            if dt and title:
                 self.pq.put([time.time()+dt, title, context])
                 return str(time.time()+dt)+" "+title
+            elif dt and not title.strip():
+                return "标题不能为空"
             else:
                 return "无法识别到有效时间"
 
     def on_interval(self):
+        with open(self.path, 'wb') as f:
+            pickle.dump(list(self.pq.queue), f)
         # 如果提醒队列非空且第一个提醒到时间了就提醒用户
         while not self.pq.empty() and self.pq.queue[0][0] < time.time():
             _, title, target = self.pq.get()
