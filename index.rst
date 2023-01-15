d9a0d7366c7336396ff3b5b34a0d929ecdae57bb
Frog Chen <satgo1546@yeah.net>
2023-01-15 22:23:44 +0800

    用Sphinx生成文档
    pdoc3说自己不麻烦，Sphinx确实难用！

---
+++ index.rst
@@ -0,0 +1,19 @@
+.. ProjectName documentation master file, created by
+   sphinx-quickstart on Sun Jan 15 20:14:17 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to ProjectName's documentation!
+=======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- src/conf.py
+++ src/conf.py
@@ -1,4 +1,6 @@
-# 存储到处都要使用的全局配置。
+"""存储到处都要使用的全局配置。"""
+import os
+import sys
 
 INTERIOR = -114514
 """管理用群。调试信息将发送到此处；管理用插件也只接受来自其中的管理命令。"""
@@ -12,10 +14,12 @@ author = "Frog Chen & Akhia"
 
 extensions = ["sphinx.ext.autodoc"]
 
-templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+templates_path = ["../docs/templates"]
 
 language = "zh_CN"
 
-html_theme = "alabaster"
-html_static_path = ["_static"]
+html_theme = "basic"
+html_static_path = ["../docs/resources"]
+html_css_files = ["custom.css"]
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
