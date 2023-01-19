93e56da70e0e9c62c392e590d886d92679b415c9
Frog Chen <satgo1546@yeah.net>
2023-01-19 18:08:30 +0800

    引入主题色配置项

--- pykinezumiko/conf.py
+++ pykinezumiko/conf.py
@@ -6,6 +6,11 @@ import sys
 INTERIOR = -114514
 """管理用群。调试信息将发送到此处；管理用插件也只接受来自其中的管理命令。"""
 
+THEME = ("#000000", "#b53d00", "#ffcc80", "#fff3e0")
+"""文字色、深色前景、深色背景、浅色背景。"""
+ACCENTS = ("#b71c1c", "#827717", "#33691e", "#006064", "#0d47a1", "#4a148c")
+"""用于图表等的红黄绿青蓝紫。"""
+
 # 下列选项用于Sphinx文档生成器。
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
