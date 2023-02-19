f80957bb64753e07684a4aae351c2124cbd248ed
Frog Chen <satgo1546@yeah.net>
2023-02-19 18:06:08 +0800

    避免数据库示例与.select from冲突

--- pykinezumiko/plugins/70db.py
+++ pykinezumiko/plugins/70db.py
@@ -6,15 +6,15 @@ class 一条数据(docstore.Record):
 
 
 class CRUD(ChatbotBehavior):
-    def on_command_insert(self, k: str, v: str):
+    def on_command_crud_insert(self, k: str, v: str):
         一条数据[k] = 一条数据(text=v)
         return k
 
-    def on_command_select(self):
+    def on_command_crud_select(self):
         return "\n".join(f"{k}: {v.text}" for k, v in 一条数据.items()) if len(一条数据) else "空"
 
-    on_command_update = on_command_insert
+    on_command_crud_update = on_command_crud_insert
 
-    def on_command_delete(self, k: str):
+    def on_command_crud_delete(self, k: str):
         del 一条数据[k]
         return k
