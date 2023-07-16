f27a06846bc1aeb79de78c7ae932e971c06ea1d9
Frog Chen <satgo1546@yeah.net>
2023-07-16 18:29:14 +0800

    优化20touchfish.py

--- pykinezumiko/plugins/20touchfish.py
+++ pykinezumiko/plugins/20touchfish.py
@@ -19,30 +19,27 @@ class TouchFish(ChatbotBehavior):
     def __init__(self) -> None:
         super().__init__()
         # 存储路径
-        self.path = "logs/20touchfish.pickle"
+        self.path = "logs/20touchfish.txt"
 
-    def touch_fish(self, sender: int) -> None:
+    def on_command_touch_fish(self, sender: int) -> None:
         r = requests.get("https://api.vvhan.com/api/moyu?type=json", timeout=3)
-        print()
         # 请求摸鱼人日历API
         self.send(
             sender,
             f"{r.json()['url']}",
         )
 
-    def on_message(self, context: int, sender: int, text: str, message_id: int):
-        if text.startswith(".touchfish"):
-            self.touch_fish(sender)
-
     def on_interval(self):
         stp = time.time()
-        with open(self.path, "rb") as f:
-            last = pickle.load(f)
+        try:
+            with open(self.path, "r") as f:
+                last = int(f.readline())
+        except FileNotFoundError:
+            last = 0
 
         # 每天上午七点定时在管理群发摸鱼人日历
         if stamp2day(stp, 8) != last and stamp2hour(stp, 8) > 7:
-            self.touch_fish(conf.INTERIOR)
+            self.on_command_touch_fish(conf.INTERIOR)
             # 标记当前日期
             with open(self.path, "wb") as f:
                 pickle.dump(stamp2day(stp, 8), f)
-        pass
